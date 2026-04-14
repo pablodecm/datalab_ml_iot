086f79860177cba68465cabaac4710f6a28241bc
Pablo de Castro <pablodecm@gmail.com>
2026-04-14 22:23:11 +0200

    update model

--- 04_internet_of_wands/deployment/model.py
+++ 04_internet_of_wands/deployment/model.py
@@ -6,21 +6,16 @@ LABEL_ASSIGN = {"alohomora": 0, "lumos": 1, "wingardium-leviosa": 2, "reparo": 3
 
 
 def read_sample(predict_sample: PredictSample) -> pd.DataFrame:
-    """ "Reads the sample and returns a dataframe with the data."""
+    """Reads the sample and returns a dataframe with the data."""
 
-    # we need to transform the data into a dataframe
-    raw_df = pd.DataFrame.from_dict(predict_sample.dict())
-
-    # then we do exactly the same transformations as we do in the training
-
-    # convert to series to avoid having object elements
-    accel_df = raw_df.accel_data.apply(pd.Series).T
+    # directly create DataFrames from the sensor data dicts
+    accel_df = pd.DataFrame(predict_sample.accel_data.model_dump())
     accel_df["timestamp"] = pd.to_timedelta(accel_df.timestamp, unit="ms")
-    gyro_df = raw_df.gyro_data.apply(pd.Series).T
+    gyro_df = pd.DataFrame(predict_sample.gyro_data.model_dump())
     gyro_df["timestamp"] = pd.to_timedelta(gyro_df.timestamp, unit="ms")
 
-    # we can use the minimum of timestamp as origin to have the same reference
-    min_timestamp = min([accel_df.timestamp.min(), gyro_df.timestamp.min()])
+    # use the minimum timestamp as origin
+    min_timestamp = min(accel_df.timestamp.min(), gyro_df.timestamp.min())
 
     # set the same timestamp origin
     accel_df["timestamp"] -= min_timestamp
@@ -57,12 +52,12 @@ def extract_features(eval_df: pd.DataFrame) -> pd.DataFrame:
         f"{i}_gyro" for i in ["x", "y", "z"]
     ]
 
-    # TODO: need to adapt this to your transformations
     eval_df_mean = eval_df.mean().to_frame().T
     eval_df_mean.columns = [f"{f}_mean" for f in features]
     eval_df_std = eval_df.std(axis=0).to_frame().T
     eval_df_std.columns = [f"{f}_std" for f in features]
-    eval_df_extra = pd.concat([eval_df_mean, eval_df_std], axis=1)
+    eval_df_length = pd.DataFrame({"length": [len(eval_df)]})
+    eval_df_extra = pd.concat([eval_df_mean, eval_df_std, eval_df_length], axis=1)
 
     return eval_df_extra
 
